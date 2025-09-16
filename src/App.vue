d85e537f1adb50b6e182f9d3cb5f884dc5eaeca5
Cassie Smith <lilpainthorse@byui.edu>
2025-09-16 08:56:39 -0700

    Add canvas setup with grid and test node

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <CanvasView />
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import CanvasView from './components/CanvasView.vue' // or wherever you placed it
+</script>
